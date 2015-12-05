117c1e646cde3139e5c598ecfb67cd9fe4bd72f9
Max Wofford <max@maxwofford.com>
2015-12-05 03:27:15 -0800

    Rewrite twilio insult function

--- server.rb
+++ server.rb
@@ -2,19 +2,18 @@ require 'rubygems' # not necessary with ruby 1.9 but included for completeness
 require 'sinatra'
 require 'twilio-ruby'
 
-account_sid = ENV['TWILIO_ACCOUNT_SID']
-auth_token = ENV['TWILIO_AUTH_TOKEN']
-twilio_phone_number = ENV['TWILIO_PHONE_NUMBER']
-twiml_url = ENV['TWIML_URL']
-
-# set up a client to talk to the Twilio REST API
-@client = Twilio::REST::Client.new account_sid, auth_token
+# Twilio setup
+Twilio.configure do |config|
+  config.account_sid = ENV['TWILIO_ACCOUNT_SID']
+  config.auth_token = ENV['TWILIO_AUTH_TOKEN']
+end
 
 def insult(target_phone_number)
-  @call = @client.call.create(
-    from: twilio_phone_number,
+  @twilio = Twilio::REST::Client.new
+  @twilio.calls.create(
+    from: ENV['TWILIO_PHONE_NUMBER'],
     to: target_phone_number,
-    url: twiml_url
+    url: ENV['TWIML_URL']
   )
 end
 
@@ -26,4 +25,5 @@ end
 
 get '/insult/:number' do
   insult(params[:number])
+  "Your message to #{params[:number]} has just gone through"
 end
