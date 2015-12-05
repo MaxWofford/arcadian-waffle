4722d15b858df7c38577648a51e840b18f78ae05
Max Wofford <max@maxwofford.com>
2015-12-05 05:00:13 -0800

    Add classy insults

--- server.rb
+++ server.rb
@@ -1,19 +1,26 @@
 require 'rubygems' # not necessary with ruby 1.9 but included for completeness
 require 'sinatra'
 require 'twilio-ruby'
+require 'yaml'
+
 
 # Twilio setup
+
 Twilio.configure do |config|
   config.account_sid = ENV['TWILIO_ACCOUNT_SID']
   config.auth_token = ENV['TWILIO_AUTH_TOKEN']
 end
 
-def insult(target_phone_number)
+def insult(target_phone_number, classy)
+  @base_url ||= "#{request.env['rack.url_scheme']}://#{request.env['HTTP_HOST']}"
+  url = @base_url
+  url += params[:classy] ? '/twiml/classy' : '/twiml'
+  url += '.xml'
   @twilio = Twilio::REST::Client.new
   @twilio.calls.create(
     from: ENV['TWILIO_PHONE_NUMBER'],
     to: target_phone_number,
-    url: ENV['TWIML_URL']
+    url: url
   )
 end
 
@@ -23,7 +30,26 @@ get '/' do
   send_file File.join(settings.public_folder, 'index.html')
 end
 
-get '/insult/:number' do
-  insult(params[:number])
+post '/twiml/classy.xml' do
+  content_type 'text/xml'
+  data = YAML.load_file(File.expand_path('data/insults.yml'))
+  insult = 'Thou art a '
+  for column in data.keys
+    insult += " #{data[column][rand(data[column].length)]}"
+  end
+  Twilio::TwiML::Response.new do |r|
+    r.Say insult, voice: 'alice'
+  end.text
+end
+
+post '/twiml.xml' do
+  content_type 'text/xml'
+  Twilio::TwiML::Response.new do |r|
+    r.Say 'Fuck you'
+  end.text
+end
+
+get '/insult' do
+  insult(params[:number], params[:classy])
   "Your message to #{params[:number]} has just gone through"
 end
